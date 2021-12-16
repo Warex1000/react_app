b4930caff04b4c8297e304085e21724eff94b2c8
Artem <Lipatiev.artemii@gmail.com>
2021-12-16 22:47:16 +0200

    My React App (first commit)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react';  // import React
+import ReactDOM from 'react-dom';  // For include component to applications
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {type} from "@testing-library/user-event/dist/type";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App/>,
+    // Button with click
+    // React.createElement('button',{
+    //     onClick: () => console.log('CLICK')
+    // },'Put me'),
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
