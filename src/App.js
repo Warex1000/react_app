9aaa582214032a4c941e19be3fb6891a7178ee9a
Artem <Lipatiev.artemii@gmail.com>
2021-12-17 16:03:35 +0200

    My React App study

--- src/App.js
+++ src/App.js
@@ -1,30 +1,62 @@
 import React, {useState} from "react";
+import Counter from "./components/Counter";
 
 function App() {
 
-    const [likes, setLikes] = useState(2)
+    const [value, setValue] = useState('Text in input')
 
     // console.log(count)
     // console.log(setCount)
 
-
-  function increment() {
-      setLikes( likes + 1)
-  }
-
-  function decrement() {
-      setLikes( likes - 1)
-  }
-
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+        <Counter/>
+        <Counter/>
+        <Counter/>
+        <Counter/>
     </div>
   );
 }
 
 export default App;
 
-// 22:15
+// 26:25
+
+
+
+
+// import React, {useState} from "react";
+//
+// function App() {
+//
+//     const [likes, setLikes] = useState(2)
+//     const [value, setValue] = useState('Text in input')
+//
+//     // console.log(count)
+//     // console.log(setCount)
+//
+//
+//   function increment() {
+//       setLikes( likes + 1)
+//   }
+//
+//   function decrement() {
+//       setLikes( likes - 1)
+//   }
+//
+//   return (
+//     <div className="App">
+//       <h1>{likes}</h1>
+//       <h1>{value}</h1>
+//           <input
+//               type={'text'}
+//               value={value}
+//               onChange={event => setValue(event.target.value)}
+//           />
+//           <button onClick={increment}>Increment</button>
+//           <button onClick={decrement}>Decrement</button>
+//     </div>
+//   );
+// }
+//
+// export default App;
