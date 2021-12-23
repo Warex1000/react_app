b067050412b5b26b04db75175e7bc3cf15a0361e
Artem <Lipatiev.artemii@gmail.com>
2021-12-23 15:19:15 +0200

    My React App study

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
+import './styles/App.css';
 
 function App() {
 
@@ -11,17 +12,7 @@ function App() {
 
   return (
     <div className="App">
-        <div className="post">
-            <div className="post__content">
-                <strong>1. JavaScript</strong>
-                <div>
-                    JavaScript - Это язык програмирования
-                </div>
-            </div>
-            <div className="post__btns">
-                <button> Удалить пост</button>
-            </div>
-        </div>
+
     </div>
   );
 }
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const PostItem = () => {
+    return(
+
+        <div className="post">
+            <div className="post__content">
+                <strong>1. JavaScript</strong>
+                <div>
+                    JavaScript - Это язык програмирования
+                </div>
+            </div>
+            <div className="post__btns">
+                <button> Удалить пост</button>
+            </div>
+        </div>
+    )
+  
+}
