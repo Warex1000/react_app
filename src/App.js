4882a317fd349a29bcfdc70c7902ff0f46c57140
Artem <Lipatiev.artemii@gmail.com>
2021-12-24 19:22:41 +0200

    My React App study 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import './styles/App.css';
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -18,18 +20,38 @@ function App() {
     //     {id: 3, title: 'Python 3', body: "Description"},
     // ])
 
-  return (
+    const [title, setTitle] = useState('')
+    const bodyInputRef = useRef();
+
+    const addNewPost = (e) => {
+        e.preventDefault()
+        console.log(title)
+        console.log(bodyInputRef.current.value)
+    }
+
+    return (
     <div className="App">
         <form>
-            <input type="text" placeholder="Название поста"/>
-            <input type="text" placeholder="Описание поста"/>
-            <button>Создать пост</button>
+            {/*Управляемый компонент*/}
+            <MyInput
+                value={title}
+                onChange={e => setTitle(e.target.value)}
+                type="text"
+                placeholder="Название поста"
+            />
+            {/*Не управляемый/Не контролируемый*/}
+            <MyInput
+                ref={bodyInputRef}
+                type="text"
+                placeholder="Описание поста"
+            />
+            <MyButton onClick={addNewPost}>Создать пост</MyButton>
         </form>
         <PostList posts={posts} title="Посты про JS"/>
         {/*<PostList posts={posts2} title="Посты про Python"/>*/}
     </div>
-  );
-}
+    );
+    }
 
 export default App;
 
